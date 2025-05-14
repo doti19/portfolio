4001fe885ed65e060bfe951a11271cb6a7d3bac6
Saminas Hagos <saminassaminas@gmail.com>
2025-05-14 23:51:34 +0200

    connected prismic

---
+++ src/lib/slices/Hero/index.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import type { Content } from '@prismicio/client';
+
+	export let slice: Content.HeroSlice;
+</script>
+
+<section data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
+	<h1>
+		<span>
+
+			{slice.primary.first_name}
+		</span>
+		<span>
+
+			{slice.primary.last_name}
+		</span>
+	</h1>
+	<h2>
+		<span>
+
+			{slice.primary.tag_line}
+		</span>
+</section>
---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+
+import tailwindcss from '@tailwindcss/vite';
+
+
+export default defineConfig({
+	plugins: [
+		tailwindcss(),
+		sveltekit()],
+	server: {
+		fs: {
+			// Allow access to files from the project root.
+			allow: ['..']
+		}
+	}
+});
