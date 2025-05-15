0c1b0ea5394a3978e4e3948f840c30cd3e7631dc
Saminas Hagos <saminassaminas@gmail.com>
2025-05-15 22:42:41 +0200

    done with biography

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -4,11 +4,14 @@
 
     import IconGithub from '~icons/fa-brands/github';
 import IconLinkedin from '~icons/fa-brands/linkedin'
+	import Bounded from './Bounded.svelte';
 export let settings: Content.SettingsDocument;
 </script>
 
 
-<footer class="text-slate-600" >
+
+
+<Bounded as="footer" class="text-slate-600" >
     <div class="container mx-auto flex mt-20 flex-col items-center justify-between gap-6 py-8 sm:flex-row ">
         <div class="name flex flex-col items-center justify-center gap-x-4 gap-y-2 sm:flex-row sm:justify-self-start">
 <a href="/" class="text-x1 font-extrabold tracking-tighter
@@ -52,10 +55,10 @@ export let settings: Content.SettingsDocument;
         </div>
 
     </div>
-</footer>
-
+</Bounded>
+<!-- </div>?S -->
 <style>
-    footer {
+footer {
         
         position: absolute;
         bottom: 0;
